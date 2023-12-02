db55ec5d70126c7990fcde8e808ccc3e306996d5
Sujan Shrestha <techblish@Sujans-MacBook-Pro.local>
2023-12-02 19:52:34 +0900

    added post.controller.js

--- snap-minds-api/src/app.js
+++ snap-minds-api/src/app.js
@@ -17,10 +17,13 @@ app.use(cookieParser())
 
 //routes import
 import userRouter from './routes/user.routes.js'
+import postRouter from './routes/post.routes.js'
 
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/posts", postRouter)
+
 
 // http://localhost:8000/api/v1/users/register
 
---
+++ snap-minds-api/src/routes/post.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { createPost } from "../controllers/post.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+
+
+const router = Router()
+
+router.route("/post").post(
+    upload.fields([
+        {
+            name: "thumbnail",
+            maxCount: 1
+        }, 
+    ]),
+    createPost
+  )
+
+
+
+export default router
